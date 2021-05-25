7af5e77079a3a4e36458fb739164f82af5e4e217
lejodev <alejandro.rv97@gmail.com>
2021-05-25 13:11:13 -0500

    fix

--- app.js
+++ app.js
@@ -19,24 +19,29 @@ mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true }, () => {
 
 app.use(
   expressJWT({ secret: JWT_SECRET, algorithms: ["HS256"] }).unless({
-    path: ["/user/create", "/user/login", "https://delivery-services-1.herokuapp.com/"],
+    path: [
+      "/user/create",
+      "/user/login",
+      "https://delivery-services-1.herokuapp.com/",
+    ],
   })
 );
+
 app.use(cors());
 app.use(express.json());
 app.use("/user", user);
 app.use("/motorcyclist", motorcyclist);
+
+const PORT = process.env.PORT || 5000;
 
 if (process.env.NODE_ENV === "production") {
   // Serve a static folder
   app.use(express.static("frontend/build"));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+    res.sendFile(path.join(__dirname + "frontend/build/index.html"));
   });
 }
-
-const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
